f2e98f4ed6a59b61c488f0f97335cc54ded96be0
cesar juarez <cesar.juarez@codeimmersives.com>
2021-02-17 12:30:25 -0800

    fixes-code

--- main.test.js
+++ main.test.js
@@ -1,6 +1,39 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+let greeting = 'Hello'
+let sum = 0;
+let prod = 0;
+
+function greet (word){
+  greeting = greeting + ' ' + word;
+}
+
+function sumOfTwo (number1,number2){
+  sum = number1 + number2;
+}
+
+function multiply (number1, number2, number3){
+  prod = number1 * number2 * number3;
+}
+
+function returnWhatISay ('Hello students, how are you'){
+  return "Hello students, how are you";
+}
+
+function sayHi (personalizedgreeting){
+  return 'Hi' + personalizedgreeting ;
+}
+
+function divide (x , y){
+  return x / y;
+}
+
+
+
+function remainder (num){
+  return x / y % num;
+}
 
 
 
